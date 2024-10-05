d0ec61688bcea0638ff4990495b6fa3eb03f4422
Alan Malta Rodrigues <alan.malta@cern.ch>
2024-10-04 22:20:52 -0400

    disable debug; allow hosts; load secrets

--- HW2/website/website/settings.py
+++ HW2/website/website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#n6kks0cs$a-7k*67)k(nof$7z6&l+u97ea)nl_r8frg_mrmd1'
+# TODO: Load the application secret key in a secure way
+# Set default key only for testing purposes
+SECRET_KEY = os.getenv("SECRET_KEY", '#n6kks0cs$a-7k*67)k(norg_mrmd1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# TODO: don't run a server in debug mode
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
